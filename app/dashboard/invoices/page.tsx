17636df4a2777390ca65d8fa069ff10a3a07504d
oscar-yepes <oyepes@unal.edu.co>
2026-06-17 19:08:11 -0500

    Add files via upload

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Invoices() {
+    return (
+        <div>
+            <h1>Lista de facturas</h1>
+        </div>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+<div>
+    <h1>Hola a todos</h1>
+</div>
+)
+}
